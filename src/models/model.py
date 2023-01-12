4ba3c7876ef513df61cc66888b9cd609c4b66a15
Mihaela Nistor <mihaelanistor5>
2023-01-12 11:09:12 +0100

    add train file and model file

---
+++ src/models/model.py
@@ -0,0 +1,12 @@
+# DistilBERT
+
+from transformers import DistilBertForSequenceClassification
+import torch.nn as nn
+
+Distil_bert = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased")
+
+Distil_bert.classifier = nn.Sequential(
+                         nn.Linear(768,7),
+                         nn.Sigmoid())
+
+
