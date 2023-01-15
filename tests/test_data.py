c64dcc2b98ac0c164d90871e2474367953231482
Mihaela Nistor <mihaelanistor5>
2023-01-15 13:27:24 +0100

    add unit tests for data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,29 @@
 # # test which checks that data gets correctly loaded
 # from src.data import make_dataset
+import sys
+sys.path.append("./src/data")
+import pandas as pd 
+import torch 
+from dataset import Toxic_Dataset
+def test_data():
 
-# def test_data():
-#     dataset = make_dataset.main()
+    n_rows = 31513
+    dataset = pd.read_csv("./data/processed/train_processed.csv",nrows=100)
+    #assert len(dataset) == n_rows
 
-# if __name__ == '__main__':
-#     test_data()
+    attributes = ['id', 'comment_text', 'toxic', 'severe_toxic', 'obscene', 'threat',
+       'insult', 'identity_hate', 'total_classes', 'non_toxic']
+    assert attributes == list(dataset.columns)
+
+    Train_data = Toxic_Dataset(pd.DataFrame(dataset[['comment_text']]), pd.DataFrame(dataset[['toxic', 'severe_toxic', 'obscene', 'threat',
+       'insult', 'identity_hate', 'total_classes', 'non_toxic']]))
+
+
+    comment, label = Train_data.__getitem__(10)
+    comment = torch.tensor(comment, dtype=torch.long)
+    label = torch.tensor(label, dtype=torch.long)
+    print(type(comment.size()))
+    print(type(label.size()))
+
+if __name__ == '__main__':
+    test_data()
