626bbba848b6ca3997e5596c2f89f06acb6d84c3
Mihaela Nistor <mihaelanistor5>
2023-01-17 09:50:19 +0100

    remove debris and format the files using black

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,28 +1,56 @@
 # # test which checks that data gets correctly loaded
 # from src.data import make_dataset
 import sys
+
 sys.path.append("./src/data")
-import pandas as pd 
-import torch 
+import pandas as pd
+import torch
 from dataset import Toxic_Dataset
+
+
 def test_data():
 
     n_rows = 31513
     dataset = pd.read_csv("./data/processed/train_processed.csv")
     assert len(dataset) == n_rows, "size of rows are different"
 
-    attributes = ['id', 'comment_text', 'toxic', 'severe_toxic', 'obscene', 'threat',
-       'insult', 'identity_hate', 'total_classes', 'non_toxic']
+    attributes = [
+        "id",
+        "comment_text",
+        "toxic",
+        "severe_toxic",
+        "obscene",
+        "threat",
+        "insult",
+        "identity_hate",
+        "total_classes",
+        "non_toxic",
+    ]
     assert attributes == list(dataset.columns), "cols are different"
 
-    Train_data = Toxic_Dataset(pd.DataFrame(dataset[['comment_text']]), pd.DataFrame(dataset[['toxic', 'severe_toxic', 'obscene', 'threat',
-       'insult', 'identity_hate', 'total_classes', 'non_toxic']]))
+    Train_data = Toxic_Dataset(
+        pd.DataFrame(dataset[["comment_text"]]),
+        pd.DataFrame(
+            dataset[
+                [
+                    "toxic",
+                    "severe_toxic",
+                    "obscene",
+                    "threat",
+                    "insult",
+                    "identity_hate",
+                    "total_classes",
+                    "non_toxic",
+                ]
+            ]
+        ),
+    )
     comment, label = Train_data.__getitem__(15)
-    comment = torch.tensor(comment['input_ids'].clone().detach(), dtype=torch.long)
+    comment = torch.tensor(comment["input_ids"].clone().detach(), dtype=torch.long)
     label = torch.tensor(label, dtype=torch.long)
     assert comment.size() == torch.Size([1, 512]), "comment size is different"
     assert label.size() == torch.Size([8]), "label size is different"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_data()
--- tests/test_model.py
+++ tests/test_model.py
@@ -9,33 +9,29 @@ import numpy as np
 
 sys.path.insert(0, "src")
 
+
 def test_forward():
 
     tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
-    comments = 'Bye! Don\'t look, come or think of comming back! Tosser.'
-    # comments["comment_text"] = comments["comment_text"].map(
-    #             lambda x: tokenizer(x, padding="max_length", truncation=True, return_tensors="pt")
-    #         )
+    comments = "Bye! Don't look, come or think of comming back! Tosser."
+
     tk = tokenizer(comments, padding="max_length", truncation=True, return_tensors="pt")
-    print(tk)
-    input_ids = tk['input_ids']
-    attention_masks = tk['attention_mask']
+    input_ids = tk["input_ids"]
+    attention_masks = tk["attention_mask"]
 
     len_id = len(input_ids[0])
     labels = 7
-    expected_shape = (len_id,labels)
-    print(1)
+    expected_shape = (len_id, labels)
     print(expected_shape)
-    print(3)
-    
+
     ids = torch.tensor(input_ids, dtype=torch.long)
     mask = torch.tensor(attention_masks, dtype=torch.long)
-    
+
     model = torch.load("models/model_epoch2.pth")
-    
+
     model.eval()
-   
-    output = model(input_ids = ids.reshape(-1,1),attention_mask=mask)
-    
+
+    output = model(input_ids=ids.reshape(-1, 1), attention_mask=mask)
+
     print(output.logits.shape)
     assert expected_shape == output.logits.shape, "Different shape"
