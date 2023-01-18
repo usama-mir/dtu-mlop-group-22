f686c02c4d774bea512d443eb083b246a4db49ce
Usama Mir <usamaahmadmir@gmail.com>
2023-01-18 14:24:55 +0100

    started implementing data drifting

--- app/main.py
+++ app/main.py
@@ -31,21 +31,25 @@ classes = [
 async def basic_predict(comment: str):
 
     # Getting the JSON from the body of the request
+    try:
 
-    tk = tokenizer(comment, padding="max_length", truncation=True, return_tensors="pt")
-    input_ids = tk["input_ids"]
-    attention_masks = tk["attention_mask"]
+        tk = tokenizer(comment, padding="max_length", truncation=True, return_tensors="pt")
+        input_ids = tk["input_ids"]
+        attention_masks = tk["attention_mask"]
 
-    ids = torch.tensor(input_ids, dtype=torch.long)
-    mask = torch.tensor(attention_masks, dtype=torch.long)
+        ids = torch.tensor(input_ids, dtype=torch.long)
+        mask = torch.tensor(attention_masks, dtype=torch.long)
 
-    output = model(input_ids=ids.reshape(-1, 1), attention_mask=mask)
-    output = output.logits.detach()
+        output = model(input_ids=ids.reshape(-1, 1), attention_mask=mask)
+        output = output.logits.detach()
 
-    predicted_classes = np.array(torch.argmax(output, dim=0))
-    actual_classes = np.where(predicted_classes == 1)[0]
-    final_class_labels = []
-    for i in actual_classes:
-        final_class_labels.append(classes[i])
+        predicted_classes = np.array(torch.argmax(output, dim=0))
+        actual_classes = np.where(predicted_classes == 1)[0]
+        final_class_labels = []
+        for i in actual_classes:
+            final_class_labels.append(classes[i])
 
-    return Response(content=str(final_class_labels))
+        return Response(content=str(final_class_labels))
+    except:
+        response = "Couldn't classify the sentence"
+        return Response(content=str(response))
